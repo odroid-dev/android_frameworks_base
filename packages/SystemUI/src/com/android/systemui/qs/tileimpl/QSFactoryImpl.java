e5b63016604b017e184ddc03729e86179ebedfc8
Altaf-Mahdi <altaf.mahdi@gmail.com>
2016-09-26 05:04:03 +0200

    QS: Add Expanded Desktop QS Tile
    Change-Id: Ied2e4533151e73e4ad9152a66e60060a42f54b28
    Signed-off-by: Shubham Singh <coolsks94@gmail.com>
    
    QS: Expanded Desktop Tile Reloaded
    
    -Switches between full/off and if you have a navbar enabled also statusbar/navbar
    -Use secondary labels to show mode instead of main label
    
    Change-Id: Ie023dc37cd03bb562144feba95df2f8069c74ad9
    Signed-off-by: mydongistiny <jaysonedson@gmail.com>
    Signed-off-by: Shubham Singh <coolsks94@gmail.com>

--- packages/SystemUI/src/com/android/systemui/qs/tileimpl/QSFactoryImpl.java
+++ packages/SystemUI/src/com/android/systemui/qs/tileimpl/QSFactoryImpl.java
@@ -32,6 +32,7 @@ import com.android.systemui.qs.tiles.ColorInversionTile;
 import com.android.systemui.qs.tiles.CompassTile;
 import com.android.systemui.qs.tiles.DataSaverTile;
 import com.android.systemui.qs.tiles.DndTile;
+import com.android.systemui.qs.tiles.ExpandedDesktopTile;
 import com.android.systemui.qs.tiles.FlashlightTile;
 import com.android.systemui.qs.tiles.HotspotTile;
 import com.android.systemui.qs.tiles.IntentTile;
@@ -101,6 +102,8 @@ public class QSFactoryImpl implements QSFactory {
                 return new NfcTile(mHost);
             case "compass":
                 return new CompassTile(mHost);
+            case "expanded_desktop":
+                return new ExpandedDesktopTile(mHost);
         }
 
         // Intent tiles.
