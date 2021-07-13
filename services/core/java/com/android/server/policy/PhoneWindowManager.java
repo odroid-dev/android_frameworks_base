4b4858069978abeac82e8154ff609adc522e42b6
Chris <codewalker@hardkernel.com>
2021-07-13 15:27:53 +0900

    ODROID-G12: Remove starting dialog.
    If system is KIOSK, then remove dialog of "Tablet is starting..."
    
    Change-Id: Ibd00e2556e80c6ea95d31518acfc6cc01bc6738d

--- services/core/java/com/android/server/policy/PhoneWindowManager.java
+++ services/core/java/com/android/server/policy/PhoneWindowManager.java
@@ -7875,7 +7875,9 @@ public class PhoneWindowManager implements WindowManagerPolicy {
                     if (mContext.getPackageManager().isUpgrade()) {
                         mBootMsgDialog.setTitle(R.string.android_upgrading_title);
                     } else {
-                        mBootMsgDialog.setTitle(R.string.android_start_title);
+                        boolean mKIOSK = SystemProperties.getBoolean("kiosk_mode", false);
+                        if (!mKIOSK)
+                            mBootMsgDialog.setTitle(R.string.android_start_title);
                     }
                     mBootMsgDialog.setProgressStyle(ProgressDialog.STYLE_SPINNER);
                     mBootMsgDialog.setIndeterminate(true);
